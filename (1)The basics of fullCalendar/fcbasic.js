6a1729954d97141ca846031a70dcfacbf43f576f
Franck Mukendi <muke.apps@gmail.com>
2018-01-17 18:24:53 -0500

    first commit

---
+++ (1)The basics of fullCalendar/fcbasic.js
@@ -0,0 +1,6 @@
+// Create calendar when document is ready
+$(document).ready(function () {
+
+    // We will refer to $calendar in future code
+    var $calendar = $("#calendar").fullCalendar();
+});
